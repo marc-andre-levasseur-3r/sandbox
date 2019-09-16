af34864403944b7da30d4b7ce90e37657d5d8afc
Marc-Andre Levasseur <marc-andre.levasseur@3rein.com>
2019-09-16 16:05:39 -0400

    hello world

---
+++ hello/go.mod
@@ -0,0 +1,8 @@
+module hello
+
+go 1.13
+
+require (
+	github.com/gorilla/mux v1.7.3 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+)
---
+++ hello/hello_server.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	name := query.Get("name")
+	if name == "" {
+		name = "Guest"
+	}
+	_, _ = w.Write([]byte(fmt.Sprintf("Hello, %s\n", name)))
+}
+
+func main() {
+	// Create Server and Route Handlers
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", handler)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ":8082",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// Start Server
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Graceful Shutdown
+	waitForShutdown(srv)
+}
+
+func waitForShutdown(srv *http.Server) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive our signal.
+	<-interruptChan
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	_ = srv.Shutdown(ctx)
+
+	os.Exit(0)
+}
